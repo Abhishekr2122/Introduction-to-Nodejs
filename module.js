73f825bddab3427d209212a3c409846021f403f7
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-01-19 20:25:38 +0530

    Reading and writing to a file

--- module.js
+++ module.js
@@ -8,5 +8,10 @@
 // fs module used to handle file system (reading and writing to a file);
 const fs = require("fs");
 
+// Reading the data present in the file
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}`;
+
+fs.writeFileSync("./txt/output.txt", textOut);
